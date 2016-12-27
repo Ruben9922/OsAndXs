2ab064beb18dbf9923d10174d96ec38c5857a05f
Ruben9922 <Ruben9922@users.noreply.github.com>
2016-12-27 22:09:48 +0000

    Input player names and symbols
    Create Player class for storing player data. Add simple code to create an array of Player objects and input players' names and symbols used (e.g. O or X) for each player.

--- src/uk/co/ruben9922/osandxs/Main.java
+++ src/uk/co/ruben9922/osandxs/Main.java
@@ -1,8 +1,31 @@
 package uk.co.ruben9922.osandxs;
 
-public class Main {
+import uk.co.ruben9922.utilities.consoleutilities.InputUtilities;
 
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Welcome to Naughts and Crosses (OsAndXs) Game!");
+        System.out.println();
+
+        // Input number of players
+        final int PLAYER_COUNT = InputUtilities.inputInt(scanner, "Number of players: ", 0, 10);
+        System.out.println();
+        scanner.nextLine();
+
+        // Create array storing player data, and input names of each player and symbols for each player
+        Player[] players = new Player[PLAYER_COUNT];
+        for (int i = 0; i < PLAYER_COUNT; i++) {
+            System.out.format("Player %d name: ", i);
+            String name = scanner.nextLine();
+            System.out.format("Player %d symbol: ", i);
+            char symbol = scanner.nextLine().toUpperCase().charAt(0);
+            players[i] = new Player(name, symbol);
+            System.out.format("Created player with name %s and symbol %c\n", players[i].getName(), players[i].getSymbol());
+            System.out.println();
+        }
     }
 }
