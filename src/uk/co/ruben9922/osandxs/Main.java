403d5d30ffb3cfd38f34b038e7725d3d74e59655
Ruben9922 <Ruben9922@users.noreply.github.com>
2016-12-27 21:29:26 +0000

    Initial commit
    Initial commit, consisting of very simple command-line program.

---
+++ src/uk/co/ruben9922/osandxs/Main.java
@@ -0,0 +1,8 @@
+package uk.co.ruben9922.osandxs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Naughts and Crosses (OsAndXs) Game!");
+    }
+}
