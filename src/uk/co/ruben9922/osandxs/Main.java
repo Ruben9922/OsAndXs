701ac18d31d5940d14dfe6132e773d5fd18575dd
Ruben9922 <Ruben9922@users.noreply.github.com>
2016-12-28 16:17:35 +0000

    Check player names and symbols are each unique
    Add code to check that player names and symbols are each unique (neither has already been used by another player).

--- src/uk/co/ruben9922/osandxs/Main.java
+++ src/uk/co/ruben9922/osandxs/Main.java
@@ -17,13 +17,36 @@ public class Main {
         scanner.nextLine();
 
         // Create array storing player data, and input names of each player and symbols for each player
+        // Check that player name and symbol are each unique (neither has already been used by another player)
         Player[] players = new Player[PLAYER_COUNT];
         for (int i = 0; i < PLAYER_COUNT; i++) {
-            // TODO: Ensure name and symbol are unique
-            System.out.format("Player %d name: ", i);
-            String name = scanner.nextLine(); // TODO: Ensure a name is entered
-            System.out.format("Player %d symbol: ", i);
-            char symbol = scanner.nextLine().toUpperCase().charAt(0); // TODO: Ensure a non-whitespace character is entered
+            String name;
+            char symbol;
+            boolean unique;
+            do {
+                // May change so name and symbol checked separately
+                System.out.format("Player %d name: ", i);
+                name = scanner.nextLine(); // TODO: Ensure a name is entered
+                System.out.format("Player %d symbol: ", i);
+                symbol = scanner.nextLine().toUpperCase().charAt(0); // TODO: Ensure a non-whitespace character is entered
+
+                unique = true;
+                for (int j = 0; j < i; j++) {
+                    if (players[j].getName().equals(name)) {
+                        unique = false;
+                        System.out.println("Name is taken!");
+                        System.out.println();
+                        break;
+                    }
+                    if (players[j].getSymbol() == symbol) {
+                        unique = false;
+                        System.out.println("Symbol is taken!");
+                        System.out.println();
+                        break;
+                    }
+                }
+            } while (!unique);
+
             players[i] = new Player(name, symbol);
             System.out.format("Created player with name %s and symbol %c\n", players[i].getName(), players[i].getSymbol());
             System.out.println();
