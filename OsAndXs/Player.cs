ec2ee7955e9c659c4f9eeca820c29c7979f40c6c
Ruben9922 <Ruben9922@users.noreply.github.com>
2018-01-24 02:29:29 +0000

    Create simple Player class

---
+++ OsAndXs/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace OsAndXs
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public char Symbol { get; set; }
+
+        public Player(string name, char symbol)
+        {
+            Name = name;
+            Symbol = symbol;
+        }
+    }
+}
