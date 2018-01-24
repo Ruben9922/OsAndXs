0bb1c042389fb7619f816a57da68ecbc97a55a86
Ruben9922 <Ruben9922@users.noreply.github.com>
2018-01-24 16:05:56 +0000

    Improve drawing board so grid lines shown

--- OsAndXs/Board.cs
+++ OsAndXs/Board.cs
@@ -11,7 +11,7 @@ namespace OsAndXs
         public Board(int size)
         {
             cells = new char[size, size];
-            cells.Fill(' ');
+            cells.Fill('c');
         }
 
         public void Display()
@@ -21,8 +21,25 @@ namespace OsAndXs
             {
                 for (int j = 0; j < cells.GetLength(1); j++)
                 {
-                    Console.Write(cells[i, j]);
                     Console.Write(" ");
+                    Console.Write(cells[i, j]);
+                    if (j != cells.GetLength(1) - 1)
+                    {
+                        Console.Write(" ");
+                        Console.Write("|"); 
+                    }
+                }
+                Console.WriteLine();
+                if (i != cells.GetLength(0) - 1)
+                {
+                    for (int j = 0; j < cells.GetLength(1); j++)
+                    {
+                        Console.Write("---");
+                        if (j != cells.GetLength(1) - 1)
+                        {
+                            Console.Write("+");
+                        }
+                    }
                 }
                 Console.WriteLine();
             }
